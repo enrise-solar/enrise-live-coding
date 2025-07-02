8186035fb94dffb8e50c247eeaa2c615040fbf84
Clément Marcilhacy <clementmarcilhacy@gmail.com>
2025-07-02 12:57:45 +0200

    update data

--- src/data/sites.ts
+++ src/data/sites.ts
@@ -12,24 +12,24 @@ export const sites: Site[] = [
     id: "1",
     name: "Strauss",
     location: "160 Av. Marcou Delanglade, 84000 Avignon, France",
-    installedPower: 170,
-    plantPrice: 200000, // €
-    actualEnergyPrice: 180, // €/MWh
+    installedPower: 420,
+    plantPrice: 400000, // €
+    actualEnergyPrice: 105, // €/MWh
   },
   {
     id: "2",
     name: "Super U",
     location: "3 Rue de la Vergne, 79300 Bressuire, France",
-    installedPower: 250,
-    plantPrice: 220000, // €
-    actualEnergyPrice: 185, // €/MWh
+    installedPower: 300,
+    plantPrice: 320000, // €
+    actualEnergyPrice: 125, // €/MWh
   },
   {
     id: "3",
     name: "Flunch",
     location: "37 Av. des 2 Fontaines, 57140 Metz, France",
-    installedPower: 300,
-    plantPrice: 330000, // €
-    actualEnergyPrice: 170,
+    installedPower: 350,
+    plantPrice: 380000, // €
+    actualEnergyPrice: 115, // €/MWh
   },
 ];
